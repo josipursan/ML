b2886597fcc8fdf97922d6338d8cd79320eea340
josipursan <josip.ursan@gmail.com>
2025-05-08 15:55:28 +0200

    ML:Playground:k_means
    Code cleanup in k_means.py.

--- Playground/k_means/k_means.py
+++ Playground/k_means/k_means.py
@@ -59,31 +59,26 @@ def L2_norm(x, y, given_centroid):
     Y_movement = abs(y - given_centroid[1])
     distance = math.sqrt(X_movement**2 + Y_movement**2)
 
-    #print("x : {}\ty : {}\ngiven_centroid : {}\nX_movement : {}\nY_movement : {}\ndistance : {}\n".format(x, y, given_centroid, X_movement, Y_movement, distance))
-
     return distance
 
-def main():
-    # Number of cluster centroids
-    K = 2
-    # Generating K random colors for plotting k-means iterations
+def generate_random_colors(K):
     colors = []
     for i in range(K):
         colors.append('#%06X' % random.randint(0, 0xFFFFFF))
-    
-    #Number of datapoints
-    number_of_datapoints = 100
+    return colors
 
-    K_centroid_coords = []
+def main():
+    K = 2 # Number of cluster centroids    
+    number_of_datapoints = 100 #Number of datapoints to randomly generate
+
+    colors = [] # Stores K random colors for plotting each k-means iteration
+    K_centroid_coords = []    
+    previous_iteration_cluster_assignments = []
+
     x, y = generate_random_x_y(number_of_datapoints)
-    print("x : {}\ny : {}\n".format(x, y))
-
     xy = np.vstack((x,y)).T
     x_np_array = np.array(x)
     y_np_array = np.array(y)
-    print("Shapes\nx_np_array : {}\ny_np_array : {}".format(x_np_array.shape, y_np_array.shape))
-
-    previous_iteration_cluster_assignments = []
 
     # Randomly initialize cluster centroids - option_2 (look at the top)
     for i in range(K):
@@ -92,30 +87,28 @@ def main():
         cluster_centroid.append(random.randint(0, 101))
 
         K_centroid_coords.append(cluster_centroid)
-    print(K_centroid_coords)
 
-    #This list contains distances for all datapoints to all cluster centroids - it will be composed of K sublists : one for each cluster centroid
-    # For each centroid we compute distance to each point
     iteration = 0
     while True:
-        print("ITERATION : {}".format(iteration))
-        cluster_centroid_assignments = []
         all_distances = []
+        cluster_centroid_assignments = []
+        per_cluster_datapoint_indices = []  # list made up of K sublists, each containing indices, in x and y, for the points assigned to the K-th centroid
+        
         for centroid in range(K):
             current_centroid_distances = []
             for i in range(number_of_datapoints):
                 current_centroid_distances.append(L2_norm(x[i], y[i], K_centroid_coords[centroid]))
             all_distances.append(current_centroid_distances)
-
         all_distances_array = np.array(all_distances)
         
-        # This code block is used to determine which datapoints are closest to which cluster centroids. It uses all_distances matrix, traversing column per column, and checking which sublist has the smallest value
-        # for the current column.
+        # Determines which datapoints are closest to which centroids - "all_distances_array" has distances for all datapoints to all centroids; 
+        # Each index i of "cluster_centroid_assignments" list is possitionally mapped to datapoints (e.g. value stored in cluster_centroid_assignments[0] describes to which centroid datapoint x[0]y[0] belongs)
+        # At each index i of "cluster_centroid_assignments" a value representing centroid to which the i-th datapoint is closest can be found.
         for col in range(all_distances_array.shape[1]):
-            #min_value = np.min(all_distances_array[:, col]) # How to find the smallest value in a column
             cluster_centroid_assignments.append(np.argmin(all_distances_array[:, col]))    # How to get the row index of the smallest value in column
+            #min_value = np.min(all_distances_array[:, col]) # How to find the smallest value in a column
+
         
-        print("cluster_centroid_assignments : {}\n".format(cluster_centroid_assignments))
         if (len(previous_iteration_cluster_assignments) == 0):
             previous_iteration_cluster_assignments = cluster_centroid_assignments
         else :
@@ -125,23 +118,12 @@ def main():
             else:
                 previous_iteration_cluster_assignments = cluster_centroid_assignments
 
-        per_cluster_datapoint_indices = []  # list made up of K sublists, each containing indices, in x and y, for the points assigned to the K-th centroid
         for centroid in range(K):
             per_cluster_datapoint_indices.append([index for index,value in enumerate(cluster_centroid_assignments) if value == centroid])
-        #centroid_A_datapoint_indices = [index for index,value in enumerate(cluster_centroid_assignments) if value == 0]
-        #centroid_A_datapoint_indices = [index for index,value in enumerate(cluster_centroid_assignments) if value == 1]
 
-        print("per_cluster_datapoint_indices : \n{}\n".format(per_cluster_datapoint_indices))
         for centroid in range(K):
-            print("centroid : {}\n".format(centroid))
-            print("Centroid {} (len : {}) : \n{}\n".format(centroid, len(per_cluster_datapoint_indices[centroid]), per_cluster_datapoint_indices[centroid]))
-            print("Centroid {}\nX datapoints : \n{}\nY datapoints : \n{}\n".format(centroid, x_np_array[per_cluster_datapoint_indices[centroid]], y_np_array[per_cluster_datapoint_indices[centroid]]))
             current_centroid_datapoints = np.vstack((x_np_array[per_cluster_datapoint_indices[centroid]], y_np_array[per_cluster_datapoint_indices[centroid]])).T
-            print("\n current_centroid_datapoints : \n{}\n".format(current_centroid_datapoints))
-            print("New mean : {}\n".format( np.mean(current_centroid_datapoints, axis = 0) ))
-            print("BEFORE K_centroid_coords : \n{}\n".format(K_centroid_coords))
             K_centroid_coords[centroid] = list(np.mean(current_centroid_datapoints, axis = 0))
-            print("AFTER K_centroid_coords : \n{}\n".format(K_centroid_coords))
 
         fig = plt.figure()
         ax1 = fig.add_subplot(111)
@@ -155,7 +137,6 @@ def main():
         plt.ylabel("Y coord")
         plt.legend(loc = 'upper left')
         plt.savefig("iteration_" + str(iteration) + ".png")
-        print("\n=========================================================\n\n")
         iteration += 1
-                    
+      
 main()
