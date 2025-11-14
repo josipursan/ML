be6949ec4e82b09a70124f7759c5a84c99f3f158
Josip Ursan <josip.ursan@gmail.com>
2025-11-14 16:45:48 +0100

    airfoil_noise : expanded data wrangling script and its output.

--- Playground/airfoil_self_noise_UCI/dataWrangling.py
+++ Playground/airfoil_self_noise_UCI/dataWrangling.py
@@ -2,6 +2,18 @@ from ucimlrepo import fetch_ucirepo
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+import os
+import shutil
+
+def createDirectory(plotsDir):
+    if os.path.isdir(plotsDir):
+        newName_oldPlotsDir = plotsDir + str(time.time())
+        print("Directory {} already exists!\nNew name for the old directory : {}\n".format(plotsDir, newName_oldPlotsDir))
+        shutil.move(plotsDir, newName_oldPlotsDir)
+    try:
+        os.mkdir(plotsDir)
+    except Exception as e:
+        print("Exception caught when trying to create directory {}\nError : {}\n".format(plotsDir, e))
 
 # fetch dataset
 airfoil_self_noise = fetch_ucirepo(id=291)
@@ -10,37 +22,61 @@ airfoil_self_noise = fetch_ucirepo(id=291)
 X = airfoil_self_noise.data.features
 y = airfoil_self_noise.data.targets
 
-# variable information
-print(airfoil_self_noise.variables)
-
-print(X)
-print("\n\n")
-print(y)
-
-#X['frequency'].plot(kind='hist', bins=20)
-
-minMax = [X['frequency'].min().item(), X['frequency'].max().item()]
-#print("\nPD DF describe : {}\n".format(X.describe()))
-#agg_res = X.select_dtypes(include="number").agg(["min", "max", "avg", "median", "kurtosis", "skew"]) # one column is messing up stuff here because it is a 'Series' object, even though I am forcing dtypes=number - too lazy to add handling for this, which is why I'll write my own function for per column analysis lol
-plt.hist(X['frequency'], bins=20)
-plt.title("Frequency histogram")
-plt.xlabel("Frequency bin")
-plt.ylabel("Bin count")
-#plt.show()
-
 statsFile = open("./stats_analysis_dump.txt", "w")
 statsFile.write("stats_analysis_dump\nTIMESTAMP : {}\n\n".format(time.time()))
 statsFile.write("===============================================\n\n")
 X_columns = list(X.columns.values)
 
 for col in X_columns:
+    plotsDir = "./" + col + "_plots"
+    createDirectory(plotsDir)
+
+    tailVerdict = "EMPTY"
+    excessiveKurtosis = "EMPTY"
+    minVal = X[col].min()
+    maxVal = X[col].max()
     mean = X.loc[:, col].mean()
     median = X.loc[:, col].median()
+    variance = X[col].var()
+    stddev = X[col].std()
     skew = X.loc[:, col].skew()
-    statsFile.write("{} stats :\n\tmean : {}\n\tmedian : {}\n\tskew : {}\n".format(col, mean, median, skew))
+    kurtosis = X.loc[:, col].kurtosis()
+
+    if mean > median :
+        tailVerdict = "mean > median --> long right tail, positive skew"
+    else:
+        tailVerdict = "mean < median --> long left tail, negative skew"
+
+    excessiveKurtosis = 3 - kurtosis
+    if excessiveKurtosis > 0:
+        kurtosisVerdict = "excessiveKurtosis > 0, leptokurtosis"
+    elif excessiveKurtosis == 0:
+        kurtosisVerdict = "excessiveKurtosis == 0, mesokurtosis"
+    elif excessiveKurtosis < 0:
+        kurtosisVerdict = "excessiveKurtosis < 0, platykurtosis"
+    
+    statsFile.write("{} stats :\n\tmin : {}\n\tmax : {}\t\n\tmean : {}\n\tmedian : {}\n\tvariance : {}\n\tstddev : {}\n\tskew : {}\n\tkurtosis : {}\n\texcessiveKurtosis : {}\n\ttailVerdict : {}\n\t{}\n".format(col, minVal, maxVal, mean, median, variance, stddev, skew, kurtosis, excessiveKurtosis, tailVerdict, kurtosisVerdict))
     statsFile.write("- - - - - - - - - - - - -\n\n")
 
-    #statsFile.write("{}\n\tmean : {}\n".format(col, X.loc[:, col].mean()))
-    #print("{} mean : {}\n".format(col, X.loc[:, col].mean()))
-    
+    plt.hist(X[col])
+    plt.title(col + ' histogram')
+    plt.xlabel('Frequency bins')
+    plt.ylabel('Bin count')
+    plt.savefig(plotsDir + "/" + col + '_histogram.png')
+    plt.clf()
+
+    plt.scatter([i for i in range(0, len(X[col]))], X[col])
+    plt.title(col + ' scatter plot')
+    plt.xlabel('Individual value')
+    plt.ylabel(col)
+    plt.savefig(plotsDir + "/" + col + '_scatter.png')
+    plt.clf()
+
+    plt.violinplot(X[col])
+    plt.title(col + ' violin plot')
+    plt.xlabel(col)
+    plt.ylabel(col + ' values')
+    plt.savefig(plotsDir + "/" + col + '_violin.png')
+    plt.clf()
+
 statsFile.close()
